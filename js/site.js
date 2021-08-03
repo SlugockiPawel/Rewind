95442f8f671160c0d470d31a9488520ba9304adf
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-08-03 17:50:14 +0200

    implement reverse string functionality

--- js/site.js
+++ js/site.js
@@ -1,4 +1,50 @@
-//Call HelloWorld
-function HelloWorld() {
-    alert('Hello World');
+// controller function:
+function handleReverseStringButtonClickEvent() {
+    makeAlertBoxInvisible();
+
+    let userInput = getUserInput();
+
+    if (isEmpty(userInput)) {
+        alert('Input cannot be empty, please try again.')
+    } else {
+        const revString = reverseString(userInput);
+        displayString(revString);
+    }
+}
+
+// logic functions:
+// get user input from the page
+function getUserInput() {
+    return document.getElementById('userString').value;
+}
+
+
+// validate the string
+function isEmpty(stringInput) {
+    return stringInput.length === 0 ? true : false;
+}
+
+// reverse the string
+function reverseString(stringInput) {
+    const arr = Array.from(stringInput);
+    arr.reverse();
+
+    return arr.join('');
+}
+
+// view functions:
+// display reversed string to the user
+function displayString(stringToDisplay) {
+    const alertMessage = document.getElementById('message');
+    alertMessage.innerText = `Your reversed string is: ${stringToDisplay}`;
+
+    makeAlertBoxVisible();
+}
+
+function makeAlertBoxInvisible() {
+    document.getElementById('alert').classList.add('invisible');
+}
+
+function makeAlertBoxVisible() {
+    document.getElementById('alert').classList.remove('invisible');
 }
